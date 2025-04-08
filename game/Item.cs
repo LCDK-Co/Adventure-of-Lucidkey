331018c717e6eb8f1a83be839291933aaaec6134
FNAI\dnrla <dnrlalth@gmail.com>
2025-04-08 17:13:02 +0900

    플레이어와 물체간의 충돌판정 예제

---
+++ game/Item.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Item : Area2D
+{
+    public override void _Ready()
+    {
+        Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+    }
+
+    private void OnBodyEntered(Node body)
+    {
+        if (body is Player)
+        {
+            GD.Print("아이템 획득!");
+            QueueFree(); // 아이템 제거
+        }
+    }
+}
