cbec34d162e10c69ce73ffaadd176704d830ee16
FNAI\dnrla <dnrlalth@gmail.com>
2025-04-09 14:30:08 +0900

    적과의 충돌판정예제(충돌판정은 area에만 존재)

--- game/Enemy.cs
+++ game/Enemy.cs
@@ -1,10 +1,24 @@
 using Godot;
 using System;
 
-public partial class Enemy : Node2D
+public partial class Enemy : Area2D
 {
+
+    public override void _Ready()
+    {
+        Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+    }
     public override void _PhysicsProcess(double delta)
     {
         Position += new Vector2(-50, 0) * (float)delta; // 왼쪽으로 이동
+    }
+
+    private void OnBodyEntered(Node body)
+    {
+        if (body is Player)
+        {
+            GD.Print("잡혔다!");
+            QueueFree(); // 아이템 제거
+        }
     }
 }
