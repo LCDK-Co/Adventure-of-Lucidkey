ae4be7da830aeeca203acd4770d2a619492a4ca6
FNAI\dnrla <dnrlalth@gmail.com>
2025-04-12 17:53:49 +0900

    루시드키님이 화면 밖으로 못나가도록 설정

--- game/Player.cs
+++ game/Player.cs
@@ -9,20 +9,25 @@ public partial class Player : CharacterBody2D
 
     public override void _Ready()
     {
+        float randomX = GD.RandRange(50, 1100);  // 해상도 가로 1200 기준
+        float randomY = GD.RandRange(50, 600); // 해상도 세로 700 기준
+
+        Position = new Vector2(randomX, randomY);
+
         anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        anim.Play("walk"); // 기본 애니메이션 실행
+        anim.Play("idle"); // 기본 애니메이션 실행
     }
     public override void _PhysicsProcess(double delta)
     {
         Vector2 velocity = Vector2.Zero;
 
-        if (Input.IsActionPressed("ui_right"))
+        if (Input.IsActionPressed("ui_right") && Position.X < 1152)
             velocity.X += 1;
-        if (Input.IsActionPressed("ui_left"))
+        if (Input.IsActionPressed("ui_left") && Position.X > 0)
             velocity.X -= 1;
-        if (Input.IsActionPressed("ui_down"))
+        if (Input.IsActionPressed("ui_down") && Position.Y < 648)
             velocity.Y += 1;
-        if (Input.IsActionPressed("ui_up"))
+        if (Input.IsActionPressed("ui_up") && Position.Y > 0)
             velocity.Y -= 1;
 
         Position += velocity.Normalized() * Speed * (float)delta;
